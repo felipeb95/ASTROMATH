07c4ba702fa48bdd1b5f080e1e8aed2068b816cf
Felipe <felipee.b95@gmail.com>
2020-01-03 10:28:09 -0300

    pin web

---
+++ Web/pimath/database.php
@@ -0,0 +1,17 @@
+<?php
+
+/*define('DB_SERVER', '108.167.165.205');
+define('DB_USERNAME', 'n4j8h7t0_astro');
+define('DB_PASSWORD', 'yVj7NLvp]CSW');
+define('DB_DATABASE', 'n4j8h7t0_astromath');
+*/
+define('DB_SERVER', 'beta.inf.ucv.cl');
+define('DB_USERNAME', 'fitnet');
+define('DB_PASSWORD', 'p#q4LTUG');
+define('DB_DATABASE', 'fitnet');
+
+//$direccion = 'http://localhost:8888/public_html/'
+//$direccion = 'http://astromathapp.000webhostapp.com/'
+$direccion = 'http://pin.informaticapucv.cl/'
+//$direccion = 'http:///localhost/Astromath/'
+?>
